26e9616869bcddb64a77f051154bfb5935a780d4
sarah <sarah.m.alsadan@gmail.com>
2020-12-06 01:02:31 +0300

    2

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import flask
+from flask import request
+import pandas as pd
+
 
 app= flask.Flask(__name__)
 
 @app.route('/', methods=['GET'])
 
 def home():
+    arg= request.args["arg1"]
     return 'hello Amoooolaah'
 
 
